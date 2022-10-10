2899371bd0564606ab5ac5d24286cb7b30f0c30f
keykat <koin20000@gmail.com>
2022-10-10 17:36:50 +0900

    [feat] 1:1채팅 - 채팅 데이터 뷰에 연결 - 기본 기능까지만 구현 - 세부 디자인 논의 필요

--- presentation/src/main/java/com/kuroutine/kulture/chat/ChatViewModel.kt
+++ presentation/src/main/java/com/kuroutine/kulture/chat/ChatViewModel.kt
@@ -16,8 +16,17 @@ import javax.inject.Inject
 class ChatViewModel @Inject constructor(
     private val chatUsecase: ChatUsecase
 ) : ViewModel() {
+    private val _currentUser = MutableLiveData<FirebaseUser?>().apply { value = null }
+    val currentUser: LiveData<FirebaseUser?> = _currentUser
+
     private val _chatList = MutableLiveData<List<Chat>?>().apply { value = null }
     val chatList: LiveData<List<Chat>?> = _chatList
+
+    fun getCurrentUser() {
+        viewModelScope.launch {
+            _currentUser.value = chatUsecase.getCurrentUser()
+        }
+    }
 
     // 방에 맨 처음 입장할 때 실행.
     fun initChatRoom(compUid: String) {
---
+++ presentation/src/main/java/com/kuroutine/kulture/data/ChatViewType.kt
@@ -0,0 +1,5 @@
+package com.kuroutine.kulture.data
+
+enum class ChatViewType(val value: Int) {
+    LEFT(0), RIGHT(1)
+}
