65906f01ae2de0019ced5042a458f04dbde610d3
chan Heo <chc6823@naver.com>
2022-10-25 22:38:16 +0900

    my_frontpage.kt 클래스 이름 수정

--- presentation/src/main/java/com/kuroutine/kulture/my_frontpage.kt
+++ presentation/src/main/java/com/kuroutine/kulture/my_frontpage.kt
@@ -1,27 +1,14 @@
 package com.kuroutine.kulture
 
-import android.os.Build.VERSION_CODES.Q
 import android.os.Bundle
-import android.widget.RelativeLayout
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kuroutine.R
-import com.example.kuroutine.databinding.ActivityMainBinding
 
 
-class my_frontpage : AppCompatActivity() {
-
-        private lateinit var binding: ActivityMainBinding
+class MyFrontPage : AppCompatActivity() {
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            val view = binding.root
-            setContentView(view)
 
-            binding.apply{
-
-            }
 
         }
 
