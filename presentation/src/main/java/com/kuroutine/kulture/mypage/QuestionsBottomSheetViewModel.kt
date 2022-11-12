6929aff03e2dc7765de1d0b7d5177b950ae3d633
springlab19 <bomyi0425@konkuk.ac.kr>
2022-11-13 04:21:02 +0900

    [update] mypage

--- presentation/src/main/java/com/kuroutine/kulture/mypage/QuestionsBottomSheetViewModel.kt
+++ presentation/src/main/java/com/kuroutine/kulture/mypage/QuestionsBottomSheetViewModel.kt
@@ -19,12 +19,13 @@ class QuestionsBottomSheetViewModel @Inject constructor(): ViewModel() {
 
     fun setQuestionList() {
         _questionList.value = listOf(
-            QuestionModel(R.drawable.img1, "title1", "2022/01/01", false),
-            QuestionModel(R.drawable.img2, "title2", "2022/02/01", false),
-            QuestionModel(R.drawable.img1, "title3", "2022/03/01", true),
-            QuestionModel(R.drawable.img2, "title4", "2022/04/01", true),
-            QuestionModel(R.drawable.img1, "title5", "2022/05/01", false),
-            QuestionModel(R.drawable.img2, "title6", "2022/06/01", true),
+            // 질문사진,     질문제목,     질문등록시간,      질문상태(FALSE==질문중, TRUE==질문완료)
+            QuestionModel(R.drawable.korea2, "title1", "2022/01/01", false),
+            QuestionModel(R.drawable.ic_noimage, "title2", "2022/02/01", false),
+            QuestionModel(R.drawable.fan1, "title3", "2022/03/01", true),
+            QuestionModel(R.drawable.trad4, "title4", "2022/04/01", true),
+            QuestionModel(R.drawable.ic_noimage, "title5", "2022/05/01", false),
+            QuestionModel(R.drawable.trad6, "title6", "2022/06/01", true),
         )
     }
 }
