07ae14191859b86b80f5566c8bf088e54d41c35b
betterafter <koin12000@naver.com>
2022-11-14 01:39:20 +0900

    [feat] 추천 기능 추가

--- presentation/src/main/java/com/kuroutine/kulture/MainActivity.kt
+++ presentation/src/main/java/com/kuroutine/kulture/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_ranking, R.id.navigation_notifications,
+                R.id.navigation_home, R.id.navigation_ranking, R.id.navigation_recommend,
                 R.id.navigation_chat, R.id.navigation_myPage
             )
         )
